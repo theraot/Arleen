6f705e64c582f6cbea151b8f43abd5f35a61a629
= <theraot@gmail.com>
2014-12-24 18:39:44 -0500

    Created Project Arleen
    - Targeting .NET 2.0
    - Added OpenTK from NuGet package

---
+++ Arleen/Arleen/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arleen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
