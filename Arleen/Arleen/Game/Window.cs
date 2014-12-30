b5da423eb5c8626b88d82164d17e38cd95353df4
= <theraot@gmail.com>
2014-12-29 19:38:11 -0500

    Added Renderer

--- Arleen/Arleen/Game/Window.cs
+++ Arleen/Arleen/Game/Window.cs
@@ -1,18 +1,12 @@
 ﻿using Arleen.Rendering;
 using OpenTK;
-using OpenTK.Graphics.OpenGL;
 using System;
-using System.Collections.Generic;
-using System.Drawing;
 
 namespace Arleen.Game
 {
     public class Window : GameWindow
     {
-        private const float FLT_FarPlane = 1000.0f;
-        private const float FLT_NearPlane = 0.01f;
-        private readonly Camera _camera;
-        private readonly LinkedList<RenderSource> _renderSources;
+        private readonly Renderer _renderer;
 
         public Window()
             : this(Program.Configuration)
@@ -20,11 +14,6 @@ namespace Arleen.Game
             // Empty
         }
 
-        // ---
-        // TODO: Camera Projection
-        // ---
-        // TODO: Renderer
-        // ---
         // ===
         // TODO: world
         // ===
@@ -46,23 +35,7 @@ namespace Arleen.Game
         {
             VSync = configuration.VSync ? VSyncMode.On : VSyncMode.Off;
             Title = Program.DisplayName;
-
-            // ---
-
-            InitializeOpenGl();
-
-            // ---
-
-            _renderSources = new LinkedList<RenderSource>();
-            _camera = new Camera
-                (
-                    new ViewingVolume.Perspective
-                    {
-                        FieldOfView = 45,
-                        FarPlane = FLT_FarPlane,
-                        NearPlane = FLT_NearPlane
-                    }
-                );
+            _renderer = new Renderer();
         }
 
         protected override void OnLoad(EventArgs e)
@@ -73,7 +46,9 @@ namespace Arleen.Game
             // ---
             // TODO: Load localization
             // ---
-            // TODO: Initialize renderer
+
+            _renderer.Initialize(this);
+
             // ---
             // TODO: Load resources
             // ---
@@ -83,43 +58,8 @@ namespace Arleen.Game
 
         protected override void OnRenderFrame(FrameEventArgs e)
         {
-            GL.ClearColor(Color.Black);
-            GL.Clear(ClearBufferMask.DepthBufferBit | ClearBufferMask.ColorBufferBit);
-
-            foreach (var item in _renderSources)
-            {
-                item.Render(_camera, e.Time);
-            }
-
+            base.OnRenderFrame(e);
             SwapBuffers();
-        }
-
-        protected override void OnResize(EventArgs e)
-        {
-            GL.Viewport(0, 0, Width, Height);
-            _camera.ViewingVolume.Update(Width, Height);
-        }
-
-        private void InitializeOpenGl()
-        {
-            GL.Enable(EnableCap.Texture2D);
-            GL.Enable(EnableCap.CullFace);
-            GL.Enable(EnableCap.DepthTest);
-            GL.Enable(EnableCap.Blend); // need for fonts
-
-            //---
-
-            GL.CullFace(CullFaceMode.Back);
-
-            // ---
-            GL.BlendFunc(BlendingFactorSrc.SrcAlpha, BlendingFactorDest.OneMinusSrcAlpha);
-
-            // ---
-
-            GL.EnableClientState(ArrayCap.NormalArray);
-            GL.EnableClientState(ArrayCap.VertexArray);
-            GL.EnableClientState(ArrayCap.TextureCoordArray);
-            GL.EnableClientState(ArrayCap.ColorArray);
         }
     }
 }
---
+++ Arleen/Arleen/Rendering/Renderer.cs
@@ -0,0 +1,85 @@
+﻿using Arleen.Game;
+using OpenTK.Graphics.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Arleen.Rendering
+{
+    public class Renderer
+    {
+        private const float FLT_FarPlane = 1000.0f;
+        private const float FLT_NearPlane = 0.01f;
+        private readonly Camera _camera;
+        private readonly LinkedList<RenderSource> _renderSources;
+
+        public Renderer()
+        {
+            _renderSources = new LinkedList<RenderSource>();
+            _camera = new Camera
+                (
+                    new ViewingVolume.Perspective
+                    {
+                        FieldOfView = 45,
+                        FarPlane = FLT_FarPlane,
+                        NearPlane = FLT_NearPlane
+                    }
+                );
+        }
+
+        public LinkedList<RenderSource> RenderSources
+        {
+            get
+            {
+                return _renderSources;
+            }
+        }
+
+        public void Initialize(Window window)
+        {
+            InitializeOpenGl();
+            window.Resize += window_Resize;
+            window.RenderFrame += window_RenderFrame;
+        }
+
+        private void window_RenderFrame(object sender, OpenTK.FrameEventArgs e)
+        {
+            GL.ClearColor(Color.BlanchedAlmond);
+            GL.Clear(ClearBufferMask.DepthBufferBit | ClearBufferMask.ColorBufferBit);
+
+            foreach (var item in _renderSources)
+            {
+                item.Render(_camera, e.Time);
+            }
+        }
+
+        private void InitializeOpenGl()
+        {
+            GL.Enable(EnableCap.Texture2D);
+            GL.Enable(EnableCap.CullFace);
+            GL.Enable(EnableCap.DepthTest);
+            GL.Enable(EnableCap.Blend); // need for fonts
+
+            //---
+
+            GL.CullFace(CullFaceMode.Back);
+
+            // ---
+            GL.BlendFunc(BlendingFactorSrc.SrcAlpha, BlendingFactorDest.OneMinusSrcAlpha);
+
+            // ---
+
+            GL.EnableClientState(ArrayCap.NormalArray);
+            GL.EnableClientState(ArrayCap.VertexArray);
+            GL.EnableClientState(ArrayCap.TextureCoordArray);
+            GL.EnableClientState(ArrayCap.ColorArray);
+        }
+
+        private void window_Resize(object sender, EventArgs e)
+        {
+            var window = (Window)sender;
+            GL.Viewport(0, 0, window.Width, window.Height);
+            _camera.ViewingVolume.Update(window.Width, window.Height);
+        }
+    }
+}
