cf61e07219c94769b7d9915e6bd85dccfe2e9c3e
= <theraot@gmail.com>
2014-12-31 03:10:06 -0500

    Added CustomRenderer

---
+++ Arleen/Arleen/Rendering/Sources/CustomRenderer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Arleen.Rendering.Sources
+{
+    public class CustomRenderer : RenderSource
+    {
+        private readonly Action _initialize;
+        private readonly Action<RenderInfo> _render;
+
+        public CustomRenderer(Action<RenderInfo> render, Action initialize)
+        {
+            _render = render ??
+                (
+                    _ =>
+                    {
+                        //Empty
+                    }
+                );
+            _initialize = initialize ??
+                (
+                    () =>
+                    {
+                        //Empty
+                    }
+                );
+        }
+
+        public CustomRenderer(Action<RenderInfo> render)
+        {
+            _render = render ??
+                (
+                    _ =>
+                    {
+                        //Empty
+                    }
+                );
+            _initialize =
+                (
+                    () =>
+                    {
+                        //Empty
+                    }
+                );
+        }
+
+        protected override void OnInitilaize()
+        {
+            _initialize();
+        }
+
+        protected override void OnRender(RenderInfo renderInfo)
+        {
+            _render(renderInfo);
+        }
+    }
+}
