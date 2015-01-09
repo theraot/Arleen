aa356ffe634fa1fbc801937230c3fb81d3186e99
= <theraot@gmail.com>
2015-01-09 14:46:27 -0500

    Updated TransformationHelper
    Added new Apply method

--- Arleen/Arleen/Rendering/TransformationHelper.cs
+++ Arleen/Arleen/Rendering/TransformationHelper.cs
@@ -5,6 +5,12 @@ namespace Arleen.Rendering
 {
     public static class TransformationHelper
     {
+        public static void Apply(this Transformation transformation)
+        {
+            var matrix = transformation.Matrix;
+            GL.MultMatrix(ref matrix);
+        }
+
         public static void Place(this Transformation transformation)
         {
             var matrix = transformation.Matrix;
