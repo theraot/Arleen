333ea6323f63e0d3af0ff636d2ee30cf39519748
= <theraot@gmail.com>
2015-01-11 06:54:14 -0500

    Comments on TransformationHelper

--- Arleen/Arleen/Rendering/TransformationHelper.cs
+++ Arleen/Arleen/Rendering/TransformationHelper.cs
@@ -3,14 +3,25 @@ using OpenTK.Graphics.OpenGL;
 
 namespace Arleen.Rendering
 {
+    /// <summary>
+    /// Helper methods for the Transformation class
+    /// </summary>
     public static class TransformationHelper
     {
+        /// <summary>
+        /// Applies a transformation, extending any prior transformation, to the current graphic context.
+        /// </summary>
+        /// <param name="transformation">The transformation to apply.</param>
         public static void Apply(this Transformation transformation)
         {
             var matrix = transformation.Matrix;
             GL.MultMatrix(ref matrix);
         }
 
+        /// <summary>
+        /// Places a transformation, replacing any prior transformation, to the current graphic context
+        /// </summary>
+        /// <param name="transformation">The transformation to place.</param>
         public static void Place(this Transformation transformation)
         {
             var matrix = transformation.Matrix;
