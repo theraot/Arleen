cdfa56a1fb16fcb2633f710b85dfb0fabb923518
= <theraot@gmail.com>
2015-01-13 16:02:19 -0500

    Comments on FpsCounter

--- Arleen/Arleen/Rendering/FpsCounter.cs
+++ Arleen/Arleen/Rendering/FpsCounter.cs
@@ -2,12 +2,18 @@ using System;
 
 namespace Arleen.Rendering
 {
+    /// <summary>
+    /// Counts the number of frames per second
+    /// </summary>
     internal sealed class FpsCounter
     {
         private int _fpsCount;
         private int _fpsLast;
         private double time;
 
+        /// <summary>
+        /// The last recorded number of frames per second.
+        /// </summary>
         public int Fps
         {
             get
@@ -16,6 +22,10 @@ namespace Arleen.Rendering
             }
         }
 
+        /// <summary>
+        /// Adds a frame to the count, and verifies if a second has passed.
+        /// </summary>
+        /// <param name="elapsedSeconds">The fraction of seconds that has passed since the last call.</param>
         public void OnRender(double elapsedSeconds)
         {
             _fpsCount++;
