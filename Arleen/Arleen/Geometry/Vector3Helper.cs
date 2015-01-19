19496bb8f8da86952190b9d103d45d7e287080ec
= <theraot@gmail.com>
2015-01-19 17:56:02 -0500

    Comments on Vector3*Helper

--- Arleen/Arleen/Geometry/Vector3Helper.cs
+++ Arleen/Arleen/Geometry/Vector3Helper.cs
@@ -3,8 +3,18 @@ using System;
 
 namespace Arleen.Geometry
 {
+    /// <summary>
+    /// Helper methods for the Vector3 class
+    /// </summary>
     public static class Vector3Helper
     {
+        /// <summary>
+        /// Creates a vector from length, bearing and elevation.
+        /// </summary>
+        /// <param name="length">The length of the vector.</param>
+        /// <param name="bearing">The angle from the north over the horizontal plane.</param>
+        /// <param name="elevation">The angle from the horizontal plane.</param>
+        /// <returns>A new vector created with the given length, bearing and elevation.</returns>
         public static Vector3 CreateGeo(float length, float bearing, float elevation)
         {
             var cosElevationLength = (float)Math.Cos(elevation) * length;
--- Arleen/Arleen/Geometry/Vector3dHelper.cs
+++ Arleen/Arleen/Geometry/Vector3dHelper.cs
@@ -3,8 +3,18 @@ using System;
 
 namespace Arleen.Geometry
 {
+    /// <summary>
+    /// Helper methods for the Vector3d class
+    /// </summary>
     public static class Vector3dHelper
     {
+        /// <summary>
+        /// Creates a vector from length, bearing and elevation.
+        /// </summary>
+        /// <param name="length">The length of the vector.</param>
+        /// <param name="bearing">The angle from the north over the horizontal plane.</param>
+        /// <param name="elevation">The angle from the horizontal plane.</param>
+        /// <returns>A new vector created with the given length, bearing and elevation.</returns>
         public static Vector3d CreateGeo(double length, double bearing, double elevation)
         {
             var cosElevationLength = Math.Cos(elevation) * length;
