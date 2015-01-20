76b41377f5e001ac2a43834667de596604907adf
= <theraot@gmail.com>
2015-01-20 05:10:27 -0500

    Comments on ILocable

--- Arleen/Arleen/Geometry/ILocable.cs
+++ Arleen/Arleen/Geometry/ILocable.cs
@@ -1,7 +1,13 @@
 ﻿namespace Arleen.Geometry
 {
+    /// <summary>
+    /// Represents an object that can located in a 3D space.
+    /// </summary>
     public interface ILocable
     {
+        /// <summary>
+        /// Gets the current location.
+        /// </summary>
         Location Location { get; }
     }
 }
