8dee4e71235d24d3333bca7b83edb4a6dc18bafa
= <theraot@gmail.com>
2015-06-02 03:35:11 -0500

    Marked as Serializable

--- Arleen/Arleen/Rendering/RenderInfo.cs
+++ Arleen/Arleen/Rendering/RenderInfo.cs
@@ -1,11 +1,13 @@
 using Arleen.Geometry;
 using System.Drawing;
+using System;
 
 namespace Arleen.Rendering
 {
     /// <summary>
     /// Represents the information about the context of a render call.
     /// </summary>
+    [Serializable]
     public struct RenderInfo
     {
         public static RenderInfo Current;
