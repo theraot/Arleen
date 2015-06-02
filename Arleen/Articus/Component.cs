a70c4d2598815747e74ee710c51234b03a2521f8
= <theraot@gmail.com>
2015-06-02 03:26:28 -0500

    Sealed Component

--- Arleen/Articus/Component.cs
+++ Arleen/Articus/Component.cs
@@ -3,7 +3,7 @@ using System;
 namespace Articus
 {
     [Serializable]
-    public class Component
+	public sealed class Component
     {
         private readonly Type _targetType;
         private readonly string _assamblyFile;
